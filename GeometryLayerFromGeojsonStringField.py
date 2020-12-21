23502b51fa80293d69b52e19790701cfaca52e20
mkoenigb <70903958+mkoenigb@users.noreply.github.com>
2020-12-21 22:01:52 +0100

    Add files via upload

--- GeometryLayerFromGeojsonStringField.py
+++ GeometryLayerFromGeojsonStringField.py
@@ -4,29 +4,34 @@
 from PyQt5.QtCore import QCoreApplication, QVariant
 from qgis.core import (QgsJsonUtils, QgsField, QgsFeature, QgsProcessing, QgsExpression, QgsGeometry, QgsPoint, QgsFields, QgsWkbTypes,
                        QgsFeatureSink, QgsFeatureRequest, QgsProcessingAlgorithm,
-                       QgsProcessingParameterFeatureSink, QgsProcessingParameterCrs, QgsProcessingParameterField, QgsProcessingParameterFeatureSource, QgsProcessingParameterEnum, QgsProcessingParameterString, QgsProcessingParameterNumber)
+                       QgsProcessingParameterFeatureSink, QgsProcessingParameterGeometry, QgsProcessingParameterCrs, QgsProcessingParameterField, QgsProcessingParameterFeatureSource, QgsProcessingParameterEnum, QgsProcessingParameterString, QgsProcessingParameterNumber)
 
 class GeometryLayerFromGeojsonStringField(QgsProcessingAlgorithm):
     SOURCE_LYR = 'SOURCE_LYR'
     GEOJSON_FIELD = 'GEOJSON_FIELD'
-    GEOMETRYTYPE_STRING = 'GEOMETRYTYPE_STRING'
+    #GEOMETRYTYPE_STRING = 'GEOMETRYTYPE_STRING'
+    GEOMETRYTYPE_ENUM = 'GEOMETRYTYPE_ENUM'
     CRS = 'CRS'
     OUTPUT = 'OUTPUT'
+    
 
-    def initAlgorithm(self, config=None):
-        
+    def initAlgorithm(self, config=None):  
         self.addParameter(
             QgsProcessingParameterFeatureSource(
-                self.SOURCE_LYR, self.tr('Source'), [QgsProcessing.TypeVectorAnyGeometry])) # Take any source layer, unfortunately no-geometry layers will not be available...
+                self.SOURCE_LYR, self.tr('Source'), [QgsProcessing.TypeMapLayer])) # Take any source layer, unfortunately no-geometry layers will not be available...
         self.addParameter(
             QgsProcessingParameterField(
-                self.GEOJSON_FIELD, self.tr('Field containing the GeoJSON as string'),'GeoJSON','SOURCE_LYR')) # Choose the field containing the GeoJSON as string
+                self.GEOJSON_FIELD, self.tr('Field containing the GeoJSON as string'),'GeoJSON','SOURCE_LYR', 1)) # Choose the field containing the GeoJSON as string
+        #self.addParameter(
+        #    QgsProcessingParameterNumber(
+        #        self.GEOMETRYTYPE_STRING, self.tr('Geometry type of the target layer / of the GeoJSON content as number (lookup at https://qgis.org/pyqgis/3.0/core/Wkb/QgsWkbTypes.html)'),0,5)) # Unfortunately there is no WKB-Type-Input available...
         self.addParameter(
-            QgsProcessingParameterNumber(
-                self.GEOMETRYTYPE_STRING, self.tr('wkb type of geometry (lookup at https://qgis.org/pyqgis/3.0/core/Wkb/QgsWkbTypes.html)'),0,5)) # Unfortunately there is no WKB-Type-Input available...
+            QgsProcessingParameterEnum(
+                self.GEOMETRYTYPE_ENUM, self.tr('Geometry type of the target layer / of the GeoJSON content'),
+                ['Unknown','Point','LineString','Polygon','MultiPoint','MultiLineString','MultiPolygon','GeometryCollection','CircularString','CompoundCurve','CurvePolygon'],defaultValue=5)) # Only Works because these are ascending numerated in QGIS... NOT A GOOD SOLUTION!! But better than typing in a number by hand... see https://qgis.org/api/classQgsWkbTypes.html
         self.addParameter(
             QgsProcessingParameterCrs(
-                self.CRS, self.tr('CRS of the GeoJSON content'),'EPSG:4326')) # CRS of the targetlayer
+                self.CRS, self.tr('CRS of the target layer / of the GeoJSON content'),'EPSG:4326')) # CRS of the targetlayer
         self.addParameter(
             QgsProcessingParameterFeatureSink(
                 self.OUTPUT, self.tr('new_geojson_layer'))) # Output
@@ -35,7 +40,9 @@ class GeometryLayerFromGeojsonStringField(QgsProcessingAlgorithm):
         # Get Parameters and assign to variable to work with
         source_layer = self.parameterAsLayer(parameters, self.SOURCE_LYR, context)
         source_geojsonfield = self.parameterAsString(parameters, self.GEOJSON_FIELD, context)
-        wkbgeometrytype = self.parameterAsInt(parameters, self.GEOMETRYTYPE_STRING, context)
+        #wkbgeometrytype = self.parameterAsInt(parameters, self.GEOMETRYTYPE_STRING, context)
+        wkbgeometrytype_fromenum = self.parameterAsInt(parameters, self.GEOMETRYTYPE_ENUM, context)
+        wkbgeometrytype = wkbgeometrytype_fromenum # testing assignment        
         crsgeometry = self.parameterAsCrs(parameters, self.CRS, context)
         
         total = 100.0 / source_layer.featureCount() if source_layer.featureCount() else 0 # Initialize progress for progressbar
@@ -84,4 +91,4 @@ class GeometryLayerFromGeojsonStringField(QgsProcessingAlgorithm):
         return 'from_gisse'
 
     def shortHelpString(self):
-        return self.tr('This Algorithm takes a source layer containing a GeoJSON as a String in a field and creates a copy of this layer with the geometry of this field')
+        return self.tr('This Algorithm takes a source layer containing a GeoJSON as a String in a field and creates a copy of this layer with the geometry of this GeoJSON field')
